b758b37ff6ab92d2f8d0cfe8d70986b52ee1759d
Christopher Madrigal <chrisjmadrigal@gmail.com>
2023-02-22 12:34:53 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Redirect
+A small, simple HTTP redirect-only server written in Rust.
+The entire application is asynchronous and concurrent.
+
+## Usage
+Simply run the binary and provide a target URL as the first argument.
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::env::args;
+use std::process::exit;
+use async_std::net::TcpListener;
+use futures::{AsyncWriteExt, StreamExt};
+
+#[async_std::main]
+async fn main() {
+    let args: Vec<String> = args().collect();
+    let target = match args.get(1) {
+        Some(target) => target,
+        None => {
+            println!("Target URL must be specified");
+            exit(1)
+        }
+    };
+    let redirect_response = format!("HTTP/1.1 301 Moved Permanently\nLocation: {target}\nConnection: close");
+    let redirect_response_bytes = redirect_response.as_bytes();
+
+    let listener = TcpListener::bind("localhost:80").await.unwrap();
+    listener
+        .incoming()
+        .for_each_concurrent(None, |client| async {
+            match client {
+                Ok(mut stream) => {
+                    let _ = stream.write_all(redirect_response_bytes).await;
+                    let _ = stream.flush().await;
+                    let _ = stream.close().await;
+                },
+                _ => {}
+            }
+        })
+        .await;
+}
