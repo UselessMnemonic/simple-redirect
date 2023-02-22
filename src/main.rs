4318b50d19dd780dba8e7466a6ae7a2c9c6b03d5
Christopher Madrigal <chrisjmadrigal@gmail.com>
2023-02-22 14:13:22 -0800

    update bind

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,11 @@ use futures::{AsyncWriteExt, StreamExt};
 #[async_std::main]
 async fn main() {
     let args: Vec<String> = args().collect();
-    let target = match args.get(1) {
-        Some(target) => target,
-        None => {
-            println!("Target URL must be specified");
-            exit(1)
-        }
-    };
+    let target = args.get(1).expect("Target URL must be specified");
     let redirect_response = format!("HTTP/1.1 301 Moved Permanently\r\nLocation: {target}\r\nConnection: close\r\n\r\n");
     let redirect_response_bytes = redirect_response.as_bytes();
 
-    let listener = TcpListener::bind("127.0.0.1:80").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:80").await.unwrap();
     listener
         .incoming()
         .for_each_concurrent(None, |client| async {
